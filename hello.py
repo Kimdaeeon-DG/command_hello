787bb1026315f06eb670814539d4710b6c0df104
asdxl1030@naver.com <158052376+Kimdaeeon-DG@users.noreply.github.com>
2024-04-01 11:46:23 +0900

    conflict resolved Github2

--- hello.py
+++ hello.py
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 // For command line git_tutorial
-// Local repository modification
-=======
-// For command line git tutorial
-// Github modification
->>>>>>> origin/main
+// First : Github modification
+// Second : Local repository modification
 
+>>>>>>> origin/main
 print("hello world!")
 print("Tell your world")
 print("Tell his world")
