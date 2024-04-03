54ec059410a80de90fa80d680ce3902ff6c56fbd
asdxl1030@naver.com <158052376+Kimdaeeon-DG@users.noreply.github.com>
2024-04-03 09:34:32 +0900

    1st commit

--- hello.py
+++ hello.py
@@ -1,8 +1,3 @@
-// For command line git_tutorial
-// First : Github modification
-// Second : Local repository modification
-
->>>>>>> origin/main
 print("hello world!")
 print("Tell your world")
 print("Tell his world")
